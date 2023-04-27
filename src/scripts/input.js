ff0df9024cba94821c02514409afd86e9091c272
LukasW01 <lukas@wigger.one>
2023-04-27 02:05:01 +0200

    fixed error where no image got inserted

--- src/scripts/input.js
+++ src/scripts/input.js
@@ -1,4 +1,5 @@
 const input = document.getElementById("image");
+const error = document.getElementById("error");
 
 input.addEventListener("blur", () => {
     const urlRegex = /^(http|https):\/\/[^\s/$.?#].[^\s]*$/i;
@@ -7,54 +8,31 @@ input.addEventListener("blur", () => {
     if (input.value === "") {
         console.error("Input is empty");
     } else if (!urlRegex.test(input.value)) {
-        console.error("Input is not a valid URL");
+        error.innerHTML = 'Input is not a valid URL';
     } else if (!imgRegex.test(input.value)) {
-        console.error("Input does not contain an image");
+        error.innerHTML = 'Input does not contain an image';
     } else {
         console.log("Input is valid and contains an image");
+        error.innerHTML = '';
+        saveData(input.value);
+        input.value = '';
 
-        checkIfSaved(input.value);
     }
 });
 
-function checkIfSaved(url) {
-    if (navigator.userAgent.indexOf("Firefox") !== -1) {
-        browser.storage.local.get("url").then((result) => {
-            if (result.url === url) {
-                console.log("URL already saved");
-                input.value = url;
-            } else {
-                console.log("URL not saved");
-                saveData(url);
-            }
-        }).catch((error) => {
-            console.error(`Error: ${error}`)
-        });
-    } else if (navigator.userAgent.indexOf("Chrome") !== -1) {
-        chrome.storage.local.get("url", (result) => {
-            if (result.url === url) {
-                console.log("URL already saved");
-                input.value = url;
-            } else {
-                console.log("URL not saved");
-                saveData(url);
-            }
-        });
-    }
-}
 
 function saveData(url) {
     if (navigator.userAgent.indexOf("Firefox") !== -1) {
         browser.storage.local.set({url: url}).then(() => {
             console.log("Variable saved");
-            input.value = url;
+            browser.tabs.reload();
         }).catch((error) => {
             console.error(`Error: ${error}`)
         });
     } else if (navigator.userAgent.indexOf("Chrome") !== -1) {
         chrome.storage.local.set({url: url}, () => {
             console.log("Variable saved");
-            input.value = url;
+            chrome.tabs.reload();
         });
     }
 }
--- src/scripts/letterboxd.js
+++ src/scripts/letterboxd.js
@@ -1,6 +1,7 @@
 if (navigator.userAgent.indexOf("Firefox") !== -1) {
     browser.storage.local.get("url").then((result) => {
         if (result.url) {
+            console.log(`URL: ${result.url}`)
             backdrop(result.url);
         }
     }).catch((error) => {
@@ -9,23 +10,30 @@ if (navigator.userAgent.indexOf("Firefox") !== -1) {
 } else if (navigator.userAgent.indexOf("Chrome") !== -1) {
     chrome.storage.local.get("url", function(result) {
         if (result.url) {
+            console.log(`URL: ${result.url}`)
             backdrop(result.url);
         }
     });
 }
 
 function backdrop(url) {
-    const cookieData = document.cookie.match(/letterboxd\.signed\.in\.as=([^;]+)/);
+    const activeNavItem = document.querySelector('.navitem.-active');
+    const activeNavLink = activeNavItem.querySelector('.navlink');
+    const activeNavLinkText = activeNavLink.textContent;
 
-    if (cookieData && window.location.href === `https://letterboxd.com/${cookieData[1]}/`) {
+    const ulElement = document.querySelector('.subnav');
+    const firstLink = ulElement.querySelector('a').text;
+    const currentLink = window.location.href;
+
+    if (activeNavLinkText === 'Profile' && currentLink === `https://letterboxd.com/${firstLink}/`) {
         const backdropDiv = document.createElement('div');
         backdropDiv.innerHTML = `
-      <div id="backdrop" class="backdrop-wrapper -loaded" data-backdrop="${url}" data-backdropmobile="${url}" data-offset="0">
-        <div class="backdropplaceholder js-backdrop-placeholder" style="background-image: url(${url}); background-position: center -0px;"></div>
-        <div class="backdropimage js-backdrop-image" style="background-position: center 0px; background-image: url(${url});"></div>
-        <div class="backdropmask js-backdrop-fade"></div>
-      </div>
-    `;
+          <div id="backdrop" class="backdrop-wrapper -loaded" data-backdrop="${url}" data-backdropmobile="${url}" data-offset="0">
+            <div class="backdropplaceholder js-backdrop-placeholder" style="background-image: url(${url}); background-position: center -0px;"></div>
+            <div class="backdropimage js-backdrop-image" style="background-position: center 0px; background-image: url(${url});"></div>
+            <div class="backdropmask js-backdrop-fade"></div>
+          </div>
+        `;
 
         const body = document.getElementsByTagName('body')[0];
         body.insertAdjacentElement('afterbegin', backdropDiv);
@@ -33,6 +41,6 @@ function backdrop(url) {
         const contentDiv = document.getElementById('content');
         contentDiv.classList.add('-backdrop');
     } else {
-        console.error(`Not on own user page or not logged in.`);
+        console.log(`Not on own user page or not logged in.`);
     }
 }
