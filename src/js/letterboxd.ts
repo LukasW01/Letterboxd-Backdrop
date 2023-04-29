953615b61a6e3574b9921138a21df4a9032e18d1
LukasW01 <lukas@wigger.one>
2023-04-29 12:08:49 +0200

    popup input complete woking on letterboxd

--- src/js/letterboxd.ts
+++ src/js/letterboxd.ts
@@ -1,33 +1,31 @@
-/*
 import polyfill from 'webextension-polyfill';
 
-async function main(): Promise<void> {
+(async () : Promise<void> => {
     try {
-        const { url } = await polyfill.storage.local.get('url');
-        const { checkbox } = await polyfill.storage.local.get('checkbox');
-        if (url && checkbox) {
-            console.log(`URL: ${url}`);
-            console.log(`Checkbox: ${checkbox}`);
-            backdrop(url);
+        const { image } = await polyfill.storage.local.get('image');
+        if (image) {
+            console.log(`Image: ${image}`);
+            backdrop(image);
         }
     } catch (error) {
         console.error(`Error: ${error}`);
     }
-}
+})();
 
-function backdrop(url: string): void {
-    const bodyElement = document.querySelector('body').classList.contains('my-own-page');
 
-    const activeNavItem = document.querySelector('.navitem.-active');
-    const activeNavLinkText = activeNavItem?.querySelector('.navlink')?.textContent;
+const backdrop = (url : string): void => {
+    const bodyElement : boolean = document.querySelector('body').classList.contains('my-own-page');
+
+    const activeNavItem : Element = document.querySelector('.navitem.-active');
+    const activeNavLinkText : string = activeNavItem?.querySelector('.navlink')?.textContent;
 
     if (activeNavLinkText === 'Profile' && bodyElement === true) {
         document.body.classList.add('backdropped', 'backdrop-loaded');
 
-        const contentDiv = document.getElementById('content');
+        const contentDiv : HTMLElement = document.getElementById('content');
         contentDiv.classList.add('-backdrop');
 
-        const backdropDiv = document.createElement('div');
+        const backdropDiv : HTMLElement = document.createElement('div');
         backdropDiv.innerHTML = `
           <div id="backdrop" class="backdrop-wrapper -loaded" data-backdrop="${url}" data-backdropmobile="${url}" data-offset="0">
             <div class="backdropplaceholder js-backdrop-placeholder" style="background-image: url(${url}); background-position: center -0px;"></div>
@@ -35,10 +33,10 @@ function backdrop(url: string): void {
             <div class="backdropmask js-backdrop-fade"></div>
           </div>
         `;
-        const body = document.getElementsByTagName('body')[0];
+        const body : HTMLBodyElement = document.getElementsByTagName('body')[0];
         body.insertAdjacentElement('afterbegin', backdropDiv);
     } else {
         console.log('Not on own user page or not logged in.');
     }
-}
-*/
+};
+
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from 'react-dom';
 import polyfill from "webextension-polyfill";
 
@@ -10,15 +10,26 @@ const App = () => {
     const [errorText, setErrorText] = useState("");
     const [error, setError] = useState(false);
     const [imageValue, setImageValue] = useState("");
+    const urlRegex: RegExp = /^https?:\/\/[^\s/$.?#]+\.ltrbxd\.com\/.+$/;
+    const imgRegex: RegExp = /.(jpeg|jpg|gif|png|webp)$/;
+
+    useEffect(async () => {
+        try {
+            const result = await polyfill.storage.local.get("image");
+            if (result.image) {
+                setImageValue(result.image);
+            }
+        } catch (error) {
+            console.error(`Error: ${error}`);
+        }
+    }, []);
 
     const handleEvent = async (): Promise<void> => {
-        const urlRegex = /^https?:\/\/[^\s/$.?#]+\.ltrbxd\.com\/.+?\.(jpeg|jpg|gif|png|webp)$/i;
-        const imgRegex = /.(jpeg|jpg|gif|png|webp)$/;
 
         if (imageValue === "") {
-            setErrorText("Input is empty");
-            setError(true);
-        } else if (!urlRegex.test(imageValue)) {
+            console.log("Input is empty");
+        }
+        else if (!urlRegex.test(imageValue)) {
             setErrorText("Input is not a valid URL from ltrbxd.com");
             setError(true);
         } else if (!imgRegex.test(imageValue)) {
@@ -49,62 +60,19 @@ const App = () => {
         }
     };
 
+
     return (
         <main>
             <div className="p-4 flex flex-col items-start text-justify justify-between mx-auto">
                 <a href="https://gitlab.com/LukasW01/Letterboxd-Backdrop" target="_blank">
                     <img src="img/letterboxd-logo.svg" alt="letterboxd"
-                         className="ml-5 h-12 w-full object-cover justify-center"/>
+                         className="ml-10 h-12 w-full object-cover justify-center"/>
                 </a>
                 <div>
                     <p className="mt-2 line-clamp-3 text-gray-400">Letterboxd Backdrop: A Browser extension that adds a
                         custom backdrop image to your Letterboxd profile without paying for Patron.</p>
                 </div>
             </div>
-            {errorText && (
-                <div
-                    role="alert"
-                    className={`rounded-md border ${
-                        error ? "text-red-500" : "text-green-500"
-                    } border-gray-800 p-4 shadow-xl bg-gray-900 animate-slide-in`}
-                >
-                    <div className="flex items-start gap-4">
-                        <div className="flex-1">
-                            <strong
-                                className={`block font-medium ${
-                                    error ? "text-red-500" : "text-green-500"
-                                }`}
-                            >
-                                {error ? "Error" : "Changes saved"}
-                            </strong>
-                            <p className="mt-1 text-sm text-gray-200">{errorText}</p>
-                        </div>
-                        <button
-                            onClick={() => {
-                                setErrorText("");
-                                setError(false);
-                            }}
-                            className="transition text-gray-400 hover:text-gray-500"
-                        >
-                            <span className="sr-only">Dismiss popup</span>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="h-6 w-6"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            )}
             <div className="mx-4 mb-2 border-gray-400 relative flex flex-col">
                 <label htmlFor="imageValue" className="block my-1.5 text-sm font-medium text-white">
                     Image URL
@@ -117,16 +85,50 @@ const App = () => {
                         placeholder="https://ltrbxd.com/image.jpg"
                         value={imageValue}
                         onChange={(e) => setImageValue(e.target.value)}
-                        onKeyUp={handleEvent}
+                        onBlur={handleEvent}
                     />
-                    <button
-                        type="button"
-                        className="py-1 px-2 ml-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                    >
-                        Delete
-                    </button>
                 </div>
             </div>
+            {errorText && (
+                <div
+                    role="alert"
+                    className={`mx-4 ${
+                        error ? "text-red-500" : "text-green-500"
+                    }  animate-slide-in flex`}
+                >
+                    <div className="gap-4 flex items-center">
+                        <div>
+                            <strong
+                                className={`font-medium ${
+                                    error ? "text-red-500" : "text-green-500"
+                                }`}
+                            >
+                                {error ? "Error" : "Changes saved"}
+                            </strong>
+                            <span className="mt-1 text-sm text-gray-200">: {errorText}</span>
+                        </div>
+                        <button
+                            onClick={() => {
+                                setErrorText("");
+                                setError(false);
+                            }}
+                            className="flex-shrink-0 transition text-gray-400 hover:text-gray-500"
+                        >
+                            <span className="sr-only">Dismiss popup</span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                className="h-6 w-6"
+                            >
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            )}
             <div className="mx-4 mb-2 border-gray-400 relative flex">
                 <p className="mt-2 line-clamp-3 text-gray-400 text-center">
                     © <span>{currentYear}</span> - <span>Lukas Wigger</span>. All rights reserved. Licensed under <a
