0ae12124439cf13b4d78b4fe636dcb4f9fe2c7f1
LukasW01 <lukas@wigger.one>
2023-05-19 16:51:55 +0200

    version 1.1: fixed an issue where no background was displayed for patron subscribers

--- src/js/letterboxd.ts
+++ src/js/letterboxd.ts
@@ -1,27 +1,26 @@
 import polyfill from "webextension-polyfill";
 
-const backdrop = (url:string):void => {
-    const bodyElement:boolean = document.querySelector('body')?.classList.contains('my-own-page') ?? false;
+const backdrop = (url: string): void => {
+    const bodyElement: boolean = document.querySelector('body')?.classList.contains('my-own-page') ?? false;
+    const activeNavItem: string | null | undefined = document.querySelector('.navitem.-active')?.querySelector('.navlink')?.textContent;
 
-    const activeNavItem:Element | null = document.querySelector('.navitem.-active');
-    const activeNavLinkText:string | null | undefined = activeNavItem?.querySelector('.navlink')?.textContent;
-
-    if (activeNavLinkText === 'Profile' && bodyElement) {
+    if (activeNavItem === 'Profile' && bodyElement) {
         document.body.classList.add('backdropped', 'backdrop-loaded');
+        document.getElementById('content')?.classList.add('-backdrop');
 
-        const contentDiv:HTMLElement | null = document.getElementById('content');
-        contentDiv?.classList.add('-backdrop');
-
-        const backdropDiv:HTMLElement = document.createElement('div');
-        backdropDiv.innerHTML = `
+        const backdropContainer: HTMLElement | null = document.querySelector('.backdrop-container') || (() => {
+            const container: HTMLDivElement = document.createElement('div');
+            container.classList.add('backdrop-container');
+            document.body.insertAdjacentElement('afterbegin', container);
+            return container;
+        })();
+        backdropContainer.innerHTML = `
           <div id="backdrop" class="backdrop-wrapper -loaded" data-backdrop="${url}" data-backdropmobile="${url}" data-offset="0">
             <div class="backdropplaceholder js-backdrop-placeholder" style="background-image: url(${url}); background-position: center -0px;"></div>
             <div class="backdropimage js-backdrop-image" style="background-position: center 0px; background-image: url(${url});"></div>
             <div class="backdropmask js-backdrop-fade"></div>
           </div>
         `;
-        const body:HTMLBodyElement | null = document.getElementsByTagName('body')[0];
-        body?.insertAdjacentElement('afterbegin', backdropDiv);
     } else {
         console.log('Not on own user page or not logged in.');
     }
@@ -29,11 +28,11 @@ const backdrop = (url:string):void => {
 
 (async (): Promise<void> => {
     try {
-        const { image } = await polyfill.storage.local.get('image');
+        const {image} = await polyfill.storage.local.get('image');
         if (image) {
             console.log(`Image: ${image}`);
             try {
-                const response:Response = await fetch(image);
+                const response: Response = await fetch(image);
                 if (response.status === 200) {
                     backdrop(image);
                 }
